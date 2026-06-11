6ba791a15eabe26dbb05f39b6d4693e5f0d32822
Sahaya Savari F <172002578+sahaya-savari@users.noreply.github.com>
2026-06-12 00:52:41 +0530

    chore: remove unused temporary scraping scripts

--- strip_scripts.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-let html = fs.readFileSync("index.html", "utf8");
-
-// Remove all <script> tags and their contents
-html = html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, "");
-
-fs.writeFileSync("index.html", html);
-console.log("Removed all script tags from index.html");
